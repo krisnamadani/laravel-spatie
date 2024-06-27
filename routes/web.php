1d0ba7dcadcee4af06abd2f8d21143d5590c6ac5
krisna <krisnamadani@gmail.com>
2024-06-27 16:42:02 +0700

    create routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Our resource routes
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
 });
 
 require __DIR__.'/auth.php';
